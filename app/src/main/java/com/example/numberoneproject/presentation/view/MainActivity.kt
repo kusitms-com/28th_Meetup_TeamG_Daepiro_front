3e3aebbdf97c8642ccf2604e159b0674c821373e
Ojongseok <whdtjr6889@naver.com>
2023-10-05 15:57:56 +0900

    [Feat #2] 내비게이션 UI 추가 및 Controller 지정

--- app/src/main/java/com/example/numberoneproject/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/numberoneproject/presentation/view/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.numberoneproject.presentation.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.numberoneproject.R
 import com.example.numberoneproject.databinding.ActivityMainBinding
 import com.example.numberoneproject.presentation.base.BaseActivity
 import com.example.numberoneproject.presentation.util.Extensions.repeatOnStarted
 import com.example.numberoneproject.presentation.viewmodel.SampleViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
@@ -18,7 +18,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         binding.viewmodel = viewmodel
 
         viewmodel.getSample()
@@ -26,7 +26,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     }
 
     override fun initView() {
-
+        initNavigation()
     }
 
     override fun subscribeUi() {
@@ -36,5 +36,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             }
         }
 
+    }
+
+    private fun initNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationBar.setupWithNavController(navController)
     }
 }
