733a912b4396d2bfa2f63bcb5e12cfae53505147
Ojongseok <whdtjr6889@naver.com>
2023-10-05 14:47:27 +0900

    [Refactor #1] MainActivity BaseActivity 상속하도록 리팩토링

--- app/src/main/java/com/example/numberoneproject/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/numberoneproject/presentation/view/MainActivity.kt
@@ -5,25 +5,36 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import com.example.numberoneproject.R
+import com.example.numberoneproject.databinding.ActivityMainBinding
+import com.example.numberoneproject.presentation.base.BaseActivity
 import com.example.numberoneproject.presentation.util.Extensions.repeatOnStarted
 import com.example.numberoneproject.presentation.viewmodel.SampleViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     val viewmodel by viewModels<SampleViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        
+        binding.viewmodel = viewmodel
 
         viewmodel.getSample()
 
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun subscribeUi() {
         repeatOnStarted {
             viewmodel.sampleResponse.collect {
                 Log.d("taag", it.toString())
             }
         }
+
     }
 }
