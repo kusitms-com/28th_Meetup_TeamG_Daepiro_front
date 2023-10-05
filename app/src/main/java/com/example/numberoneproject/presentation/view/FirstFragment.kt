238fe88b5effbac6fe7225304559bdf91488253c
Ojongseok <whdtjr6889@naver.com>
2023-10-05 15:56:52 +0900

    [Feat #2] 내비게이션 메뉴로 들어갈 fragment 생성

---
+++ app/src/main/java/com/example/numberoneproject/presentation/view/FirstFragment.kt
@@ -0,0 +1,25 @@
+package com.example.numberoneproject.presentation.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.numberoneproject.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
