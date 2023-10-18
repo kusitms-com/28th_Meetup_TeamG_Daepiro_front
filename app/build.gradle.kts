a33b718bcb0220f301e183608f02d090ba30f5a2
songseunghui <victoria9981@naver.com>
2023-10-18 20:15:18 +0900

    [Build #7] Preferences DataStore dependencies 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,5 +83,9 @@ android {
         testImplementation("junit:junit:4.13.2")
         androidTestImplementation("androidx.test.ext:junit:1.1.5")
         androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+        //Datastore
+        implementation("androidx.datastore:datastore-preferences:1.0.0")
+        implementation("androidx.datastore:datastore-preferences-core:1.0.0")
     }
 }
