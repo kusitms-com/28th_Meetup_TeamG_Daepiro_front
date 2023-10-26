f09f83a32659f31235bc75d767bf7a43b2292817
songseunghui <victoria9981@naver.com>
2023-10-26 18:08:55 +0900

    [Design #15] 에러 dialog 일부글씨 색상 변경

--- app/src/main/java/com/example/numberoneproject/presentation/view/networkerror/NetworkDialogFragment.kt
+++ app/src/main/java/com/example/numberoneproject/presentation/view/networkerror/NetworkDialogFragment.kt
@@ -1,11 +1,16 @@
 package com.example.numberoneproject.presentation.view.networkerror
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import com.example.numberoneproject.R
 import com.example.numberoneproject.databinding.FragmentNetworkDialogBinding
@@ -20,6 +25,7 @@ class NetworkDialogFragment : BaseDialogFragment<FragmentNetworkDialogBinding>(R
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupInit()
     }
 
     override fun setupInit() {
@@ -30,6 +36,7 @@ class NetworkDialogFragment : BaseDialogFragment<FragmentNetworkDialogBinding>(R
         binding.shelterBtn.setOnClickListener{
             //또다른 dialog
         }
+        setTextColor(binding.errorTxt, binding.errorTxt.text.toString(), listOf("행동요령", "대피소 조회"))
     }
 
     override fun onStart() {
@@ -42,6 +49,21 @@ class NetworkDialogFragment : BaseDialogFragment<FragmentNetworkDialogBinding>(R
         window.attributes = params
 
         window.setGravity(Gravity.CENTER)
+    }
+
+    private fun setTextColor(textView: TextView, fullText:String, wordsToColor:List<String>){
+        val color = ContextCompat.getColor(textView.context, R.color.orange_500)
+        val spannableStringBuilder = SpannableStringBuilder(fullText)
+        for(word in wordsToColor){
+            var startIndex = fullText.indexOf(word)
+            while(startIndex != -1){
+                val endIndex = startIndex + word.length
+                spannableStringBuilder.setSpan(ForegroundColorSpan(color),
+                    startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                startIndex = fullText.indexOf(word, startIndex+1)
+            }
+        }
+        textView.text = spannableStringBuilder
     }
 
 }
