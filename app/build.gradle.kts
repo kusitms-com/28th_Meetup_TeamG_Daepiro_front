eb4abcbfedec9c56b28e83c8865a11a63e51dffe
Ojongseok <whdtjr6889@naver.com>
2023-10-26 17:08:41 +0900

    [Build] Circle ImageView 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,9 @@ dependencies {
     // DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
+    // Circle ImageView
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
 
 
     implementation("androidx.core:core-ktx:1.9.0")
