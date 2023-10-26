24b2353ed0c9eef0b0b967a35b88f84238e9c69a
Ojongseok <whdtjr6889@naver.com>
2023-10-26 22:40:52 +0900

    [Feat] 카카오맵 길찾기 api 연결

--- app/src/main/java/com/example/numberoneproject/presentation/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/numberoneproject/presentation/view/home/HomeFragment.kt
@@ -1,5 +1,9 @@
 package com.example.numberoneproject.presentation.view.home
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
@@ -14,12 +18,36 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.tvTop.setOnClickListener {
-            val action = HomeFragmentDirections.actionHomeFragmentToSubFragment()
-            findNavController().navigate(action)
+        binding.btnKakaoMap.setOnClickListener {
+            searchLoadToKakaoMap()
+        }
+    }
+
+    private fun searchLoadToKakaoMap() {
+        val startLocation = Pair(37.537229,37.4979502)      // 출발지 위도, 경도
+        val endLocation = Pair(127.005515, 127.0276368)     // 도착지 위도, 경도
+
+        val url ="kakaomap://route?sp=${startLocation.first},${endLocation.first}&ep=${startLocation.second},${endLocation.second}&by=FOOT"
+        val intent =  Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        intent.addCategory(Intent.CATEGORY_BROWSABLE)
+
+        val installCheck = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requireContext().packageManager.queryIntentActivities(
+                Intent(Intent.ACTION_MAIN, null).addCategory(Intent.CATEGORY_LAUNCHER),
+                PackageManager.ResolveInfoFlags.of(PackageManager.MATCH_DEFAULT_ONLY.toLong())
+            )
+        } else {
+            requireContext().packageManager.queryIntentActivities(
+                Intent(Intent.ACTION_MAIN, null).addCategory(Intent.CATEGORY_LAUNCHER),
+                PackageManager.GET_META_DATA
+            )
         }
 
-
-
+        // 카카오맵이 설치되어 있다면 앱으로 연결, 설치되어 있지 않다면 스토어로 이동
+        if (installCheck.isEmpty()) {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=net.daum.android.map")))
+        } else {
+            startActivity(intent)
+        }
     }
 }
