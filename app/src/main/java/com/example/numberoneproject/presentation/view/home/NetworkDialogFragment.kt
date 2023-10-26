d98670789b035634c46c163e92bf08070e3e16a6
songseunghui <victoria9981@naver.com>
2023-10-26 10:01:47 +0900

    [Design] 네트워크 상태 에러시 dialog창 디자인 완료

--- app/src/main/java/com/example/numberoneproject/presentation/view/home/NetworkDialogFragment.kt
+++ app/src/main/java/com/example/numberoneproject/presentation/view/home/NetworkDialogFragment.kt
@@ -5,9 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.example.numberoneproject.R
 
-class NetworkDialogFragment : Fragment() {
+class NetworkDialogFragment : DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
