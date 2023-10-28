47ff38b3a405af791ea5124b18f157d83f825fd7
songseunghui <victoria9981@naver.com>
2023-10-28 22:20:53 +0900

    [Fix] 의존성 추가 형식 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,9 @@ dependencies {
     // Circle ImageView
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
+    //lottie library
+    implementation("com.airbnb.android:lottie:6.1.0")
+
 
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -99,5 +102,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.airbnb.android:lottie:6.1.0")
 }
