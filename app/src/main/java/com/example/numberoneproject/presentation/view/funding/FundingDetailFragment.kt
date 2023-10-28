2f195309ddef408edd5ea65bb8329c2ced66f6aa
Ojongseok <whdtjr6889@naver.com>
2023-10-28 15:52:40 +0900

    [Feat] 후원 상세화면 Navigation 뒤로가기 설정

--- app/src/main/java/com/example/numberoneproject/presentation/view/funding/FundingDetailFragment.kt
+++ app/src/main/java/com/example/numberoneproject/presentation/view/funding/FundingDetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.numberoneproject.R
 import com.example.numberoneproject.databinding.FragmentFundingDetailBinding
 import com.example.numberoneproject.presentation.base.BaseFragment
@@ -16,5 +17,12 @@ class FundingDetailFragment : BaseFragment<FragmentFundingDetailBinding>(R.layou
 
     }
 
+    override fun setupInit() {
+        binding.fragment = this@FundingDetailFragment
 
+    }
+
+    fun onClickGoBack(view: View) {
+        findNavController().navigateUp()
+    }
 }
