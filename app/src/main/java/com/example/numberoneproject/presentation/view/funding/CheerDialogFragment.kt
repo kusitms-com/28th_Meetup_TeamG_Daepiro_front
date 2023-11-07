8bdca0a117b582760ea30a88bd24f129a07a8fa7
Ojongseok <whdtjr6889@naver.com>
2023-11-07 12:14:26 +0900

    [Design] 응원메시지 보내는 다이어로그 생성

---
+++ app/src/main/java/com/example/numberoneproject/presentation/view/funding/CheerDialogFragment.kt
@@ -0,0 +1,18 @@
+package com.example.numberoneproject.presentation.view.funding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.numberoneproject.R
+import com.example.numberoneproject.databinding.FragmentCheerDialogBinding
+import com.example.numberoneproject.presentation.base.BaseDialogFragment
+import javax.inject.Inject
+
+class CheerDialogFragment @Inject constructor(
+
+): BaseDialogFragment<FragmentCheerDialogBinding>(R.layout.fragment_cheer_dialog) {
+
+}
