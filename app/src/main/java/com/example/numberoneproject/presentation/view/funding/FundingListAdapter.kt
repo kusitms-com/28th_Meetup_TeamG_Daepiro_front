8f1a1902d9bde4453d841633452e7b36fb3fe738
Ojongseok <whdtjr6889@naver.com>
2023-11-07 12:13:59 +0900

    [Design] 후원 목록 UI 수정

--- app/src/main/java/com/example/numberoneproject/presentation/view/funding/FundingListAdapter.kt
+++ app/src/main/java/com/example/numberoneproject/presentation/view/funding/FundingListAdapter.kt
@@ -1,19 +1,37 @@
 package com.example.numberoneproject.presentation.view.funding
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver.OnGlobalFocusChangeListener
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.numberoneproject.databinding.ItemFundingListBinding
+import kotlin.math.roundToInt
 
 class FundingListAdapter: RecyclerView.Adapter<FundingListAdapter.CustomViewHolder>() {
     private lateinit var itemClickListener: OnItemClickListener
 
     inner class CustomViewHolder(private val binding: ItemFundingListBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(item: Any) {
+            var progressBarWidth = 0
+
+            binding.pbFunding.viewTreeObserver.addOnGlobalLayoutListener {
+                progressBarWidth = binding.pbFunding.width
+
+                val newMarginLeft = (binding.pbFunding.progress * progressBarWidth / binding.pbFunding.max).toFloat()
+                val params = binding.ivChar.layoutParams as ConstraintLayout.LayoutParams
+                params.leftMargin = if (newMarginLeft <10) newMarginLeft.roundToInt() else newMarginLeft.roundToInt() - 20
+                binding.ivChar.layoutParams = params
+            }
+
 
         }
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FundingListAdapter.CustomViewHolder {
         val view = ItemFundingListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CustomViewHolder(view)
