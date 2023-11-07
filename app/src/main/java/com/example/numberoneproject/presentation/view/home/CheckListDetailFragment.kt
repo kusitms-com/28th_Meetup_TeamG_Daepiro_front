543ca8651c57aea49de6c8adfd30da50eb479ee2
Ojongseok <whdtjr6889@naver.com>
2023-11-07 16:47:09 +0900

    [Feat] 행동요령 체크리스트 상세화면 추가

---
+++ app/src/main/java/com/example/numberoneproject/presentation/view/home/CheckListDetailFragment.kt
@@ -0,0 +1,17 @@
+package com.example.numberoneproject.presentation.view.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.numberoneproject.R
+import com.example.numberoneproject.databinding.FragmentCheckListDetailBinding
+import com.example.numberoneproject.presentation.base.BaseFragment
+import javax.inject.Inject
+
+class CheckListDetailFragment @Inject constructor(
+
+): BaseFragment<FragmentCheckListDetailBinding>(R.layout.fragment_check_list_detail) {
+
+}
