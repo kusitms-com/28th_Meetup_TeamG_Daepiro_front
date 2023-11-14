69e0d98c4498be437e71406c27537e736e808760
Ojongseok <whdtjr6889@naver.com>
2023-11-14 12:06:58 +0900

    [Rename] news에서 family로 파일명 수정

---
+++ app/src/main/java/com/daepiro/numberoneproject/presentation/view/family/FamilyFragment.kt
@@ -0,0 +1,21 @@
+package com.daepiro.numberoneproject.presentation.view.family
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.daepiro.numberoneproject.R
+import com.daepiro.numberoneproject.databinding.FragmentFamilyBinding
+import com.daepiro.numberoneproject.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FamilyFragment : BaseFragment<FragmentFamilyBinding>(R.layout.fragment_family) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
