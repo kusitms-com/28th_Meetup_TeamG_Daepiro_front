514f9b38b7e6530f9bf91eb50a15c43d16700273
Ojongseok <whdtjr6889@naver.com>
2023-11-15 21:46:18 +0900

    [Design] 시스템 위치 팝업에 텍스트 강조

--- app/src/main/java/com/daepiro/numberoneproject/presentation/view/home/LocationOffDialogFragment.kt
+++ app/src/main/java/com/daepiro/numberoneproject/presentation/view/home/LocationOffDialogFragment.kt
@@ -7,11 +7,15 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.daepiro.numberoneproject.R
 import com.daepiro.numberoneproject.databinding.FragmentLocationOffDialogBinding
 import com.daepiro.numberoneproject.presentation.base.BaseDialogFragment
@@ -35,6 +39,19 @@ class LocationOffDialogFragment : BaseDialogFragment<FragmentLocationOffDialogBi
             ActivityCompat.finishAffinity(requireActivity())
             exitProcess(0)
         }
+
+        val originalText = getString(R.string.대피로에서_제공하는_서비스를_이용하기_위해_).format("항상 허용")
+        val spannableString = SpannableString(originalText)
+        // 문자열에서 강조할 부분의 시작 인덱스 찾기
+        val startIndex = originalText.indexOf("항상 허용")
+
+        spannableString.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.orange_500)),
+            startIndex,
+            startIndex + "항상 허용".length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        binding.tvDescription.text = spannableString
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
