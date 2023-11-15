e690915fe770ea48d49159fc1ed61f456b90fb56
Ojongseok <whdtjr6889@naver.com>
2023-11-15 19:15:24 +0900

    [Design] 홈 진입할 때 위치 꺼져있으면 뜨는 다이어로그 UI 수정

--- app/src/main/java/com/daepiro/numberoneproject/presentation/view/home/LocationOffDialogFragment.kt
+++ app/src/main/java/com/daepiro/numberoneproject/presentation/view/home/LocationOffDialogFragment.kt
@@ -1,9 +1,16 @@
 package com.daepiro.numberoneproject.presentation.view.home
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import com.daepiro.numberoneproject.R
 import com.daepiro.numberoneproject.databinding.FragmentLocationOffDialogBinding
@@ -15,14 +22,30 @@ class LocationOffDialogFragment : BaseDialogFragment<FragmentLocationOffDialogBi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         this.isCancelable = false
 
-        binding.tvGo.setOnClickListener {
+        binding.tvLocationOn.setOnClickListener {
             startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            ActivityCompat.finishAffinity(requireActivity())
+            exitProcess(0)
+        }
+
+        binding.tvClose.setOnClickListener {
             ActivityCompat.finishAffinity(requireActivity())
             exitProcess(0)
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onResume() {
+        super.onResume()
+        val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
+        val windowManager = requireActivity().getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val size = windowManager.currentWindowMetrics
+        val deviceWidth = size.bounds.width()
 
+        params?.width = (deviceWidth * 0.8).toInt()
+        dialog?.window?.attributes = params as WindowManager.LayoutParams
+    }
 }
