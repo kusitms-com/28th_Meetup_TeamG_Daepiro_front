54c15fa6ad523efd0c767b4a7839283383487838
songseunghui <victoria9981@naver.com>
2023-11-16 23:22:13 +0900

    [Fix #27] api 변경에 따른 데이터 모델 변경

--- app/src/main/java/com/daepiro/numberoneproject/data/model/CommunityTownDetailData.kt
+++ app/src/main/java/com/daepiro/numberoneproject/data/model/CommunityTownDetailData.kt
@@ -3,14 +3,17 @@ package com.daepiro.numberoneproject.data.model
 data class CommunityTownDetailData(
     val address: String="",
     val articleId: Int =0,
+    val articleTag:String="",
     val content: String="",
     val createdAt: String="",
     val imageUrls: List<String>? = null,
     val likeCount: Int=0,
-    val memberName: String="",
-    val memberNickName: String="",
+    val ownerName: String="",
+    val ownerNickName: String="",
     val modifiedAt: String="",
     val ownerMemberId: Int=0,
+    val ownerProfileImageUrl : String="",
     val thumbNailImageUrl: String = "",
+    val liked : Boolean = true,
     val title: String=""
 )
